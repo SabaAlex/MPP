977aee3c3b379e2e58526fa0ef532455aed1804d
Catalin Retegan <catalin.retegan@gmail.com>
2020-03-02 22:36:35 +0200

    ClientInMemoryRepoTest

---
+++ src/test/java/repository/ClientInMemoryRepositoryTest.java
@@ -0,0 +1,54 @@
+package repository;
+
+import model.validators.ClientValidator;
+import model.validators.Validator;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+
+public class ClientInMemoryRepositoryTest {
+
+    @Ignore
+    @Test
+    public void testFindOne() throws Exception {
+        fail("Not yet tested");
+    }
+
+    @Ignore
+    @Test
+    public void testFindAll() throws Exception {
+        fail("Not yet tested");
+    }
+
+    @Ignore
+    @Test
+    public void testSave() throws Exception {
+        fail("Not yet tested");
+    }
+
+    @Ignore
+    //@Test(expected = ClientValidator)
+    public void testSaveException() throws Exception {
+        fail("Not yet tested");
+    }
+
+    @Ignore
+    @Test
+    public void testDelete() throws Exception {
+        fail("Not yet tested");
+    }
+
+    @Ignore
+    @Test
+    public void testUpdate() throws Exception {
+        fail("Not yet tested");
+    }
+
+    @Ignore
+   // @Test(expected = ValidatorException.class)
+    public void testUpdateException() throws Exception {
+        fail("Not yet tested");
+    }
+}
