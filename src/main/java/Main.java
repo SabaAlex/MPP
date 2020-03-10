f00fd216d88be95581fca06f44d17c6bfe536989
Alex Sabadus <sabadusalex@gmail.com>
2020-03-10 10:01:03 +0200

    Resource Folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,29 @@ import model.domain.Client;
 import model.domain.Movie;
 import model.validators.ClientValidator;
 import model.validators.MovieValidator;
+import repository.ClientFileRepository;
 import repository.IRepository;
 import repository.InMemoryRepository;
+import repository.MovieFileRepository;
+
+
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         ClientValidator clientValidator = new ClientValidator();
-        IRepository<Long, Client> inMemoryRepositoryClient = new InMemoryRepository(clientValidator);
 
+        ///IRepository<Long, Client> inMemoryRepositoryClient = new ClientFileRepository(clientValidator, Paths.get("C:\\University\\MPP\\projectResources\\Clients.txt").toString());
+
+
+        IRepository<Long, Client> inMemoryRepositoryClient = new InMemoryRepository<>(clientValidator);
         MovieValidator movieValidator = new MovieValidator();
-        IRepository<Long, Movie> inMemoryRepositoryMovie = new InMemoryRepository(movieValidator);
 
+        ///IRepository<Long, Movie> inMemoryRepositoryMovie= new MovieFileRepository(movieValidator, Paths.get("C:\\University\\MPP\\projectResources\\Clients.txt").toString());
+
+        IRepository<Long, Movie> inMemoryRepositoryMovie = new InMemoryRepository<>(movieValidator);
         ClientService clientService = new ClientService(inMemoryRepositoryClient);
 
         MovieService movieService = new MovieService(inMemoryRepositoryMovie);
