9992e210dab05b02026004ea33151baf839cfa76
Alex Sabadus <sabadusalex@gmail.com>
2020-06-16 23:53:25 +0300

    asd

--- web/src/main/java/app/config/Initializer.java
+++ web/src/main/java/app/config/Initializer.java
@@ -1,25 +1,18 @@
 package app.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+@SpringBootConfiguration
+public class Initializer {
 
-
-public class Initializer implements WebApplicationInitializer {
-
-    @Override
-    public void onStartup(ServletContext container)
-            throws ServletException {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.scan("app");
-
-        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(context));
-        dispatcher.addMapping("api");
-        dispatcher.setLoadOnStartup(1);
-
+    @Bean
+    public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
+        registration.addUrlMappings("/api/*");
+        return registration;
     }
+
 }
